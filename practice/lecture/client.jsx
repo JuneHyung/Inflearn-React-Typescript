edc44a42fd4fca3f788e208624697c5f8eaa918a
JUNEHYUNG\USER <cjh951114@gmail.com>
2023-10-16 14:59:49 +0900

    00. 구구단, 끝말잇기 : ~1.2 , project 이동

--- practice/lecture/client.jsx
+++ practice/lecture/client.jsx
@@ -1,6 +1,4 @@
-const React = require('react');
-const ReactDom = require('react-dom');
+import * as React from 'react';
+import * as ReactDOM from 'react';
 
-const WordRelay = require('./WordRelay')
-
-ReactDom.render(<WordRelay />, documnet.querySelector('#root'))
+ReactDOM.render(<GuGuDan />)
