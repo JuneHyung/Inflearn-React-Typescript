d2d610caf09671460db1f54bb4bcc88d117ae146
JUNEHYUNG\USER <cjh951114@gmail.com>
2023-10-23 19:48:06 +0900

    03. 틱택토

--- practice/lecture/client.tsx
+++ practice/lecture/client.tsx
@@ -6,11 +6,13 @@ const ReactDOM = require('react-dom');
 // const NumberBaseball = require('./NumberBaseball');
 // const ResponseCheck = require('./ResponseCheck');
 // const RSP = require('./RSP');
-const Lotto = require('./Lotto');
+// const Lotto = require('./Lotto');
+const TicTacToe = require('./tictactoe/TicTacToe.jsx');
 
 
 // ReactDOM.render(<GuGuDan />, document.querySelector('#root'))
 // ReactDOM.render(<WordRelay />, document.querySelector('#root'))
 // ReactDOM.render(<NumberBaseball />, document.querySelector('#root'))
 // ReactDOM.render(<RSP />, document.querySelector('#root'))
-ReactDOM.render(<Lotto />, document.querySelector('#root'))
+// ReactDOM.render(<Lotto />, document.querySelector('#root'))
+ReactDOM.render(<TicTacToe />, document.querySelector('#root'))
